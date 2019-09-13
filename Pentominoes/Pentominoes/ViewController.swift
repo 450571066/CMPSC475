a41e0c09ce2ee8762d00691073fb43e05e79fd0b
450571066 <tzl60@psu.edu>
2019-09-13 16:37:48 -0400

    Set UI and change board image

--- Pentominoes/Pentominoes/ViewController.swift
+++ Pentominoes/Pentominoes/ViewController.swift
@@ -15,6 +15,29 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var mainBoard: UIImageView!
+    @IBAction func Button_1(_ sender: Any) {
+        mainBoard.image = UIImage(named: "Board0")
+    }
+    
+    @IBAction func Button_2(_ sender: Any) {
+        mainBoard.image = UIImage(named: "Board1")
+    }
+    
+    @IBAction func Button_3(_ sender: Any) {
+        mainBoard.image = UIImage(named: "Board2")
+    }
+    
+    @IBAction func Button_4(_ sender: Any) {
+        mainBoard.image = UIImage(named: "Board3")
+    }
+    
+    @IBAction func Button_5(_ sender: Any) {
+        mainBoard.image = UIImage(named: "Board4")
+    }
+    
+    @IBAction func Button_6(_ sender: Any) {
+        mainBoard.image = UIImage(named: "Board5")
+    }
 }
 
